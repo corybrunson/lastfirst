f029b2bf315e6f06e5cb57d503e7129bc4225d83
Jason Cory Brunson <jason.brunson@medicine.ufl.edu>
2022-10-28 07:46:11 -0400

    increase allocated time

---
+++ HPG/knn-mimic-1-gower-micu.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#SBATCH --job-name=kmg_micu   # Job name
+#SBATCH --mail-type=ALL             # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=jason.brunson@medicine.ufl.edu
+#SBATCH --nodes=1                   # Use one node
+#SBATCH --ntasks=1                  # Run a single task
+#SBATCH --cpus-per-task=1           # Use 1 core
+#SBATCH --mem=2400mb                # Memory limit
+#SBATCH --time=48:00:00             # Time limit hrs:min:sec
+#SBATCH --output=knn-mimic-1-gower-micu.out # Std output and error log
+
+pwd; hostname; date
+
+module load R
+
+echo "Running `kmg_micu` on one core..."
+
+Rscript ~/lastfirst/scripts/knn-mimic-1-gower-micu.r
+
+date
---
+++ HPG/knn-mimic-1-gower-tsicu.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#SBATCH --job-name=kmg_tsicu  # Job name
+#SBATCH --mail-type=ALL             # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=jason.brunson@medicine.ufl.edu
+#SBATCH --nodes=1                   # Use one node
+#SBATCH --ntasks=1                  # Run a single task
+#SBATCH --cpus-per-task=1           # Use 1 core
+#SBATCH --mem=600mb                # Memory limit
+#SBATCH --time=48:00:00             # Time limit hrs:min:sec
+#SBATCH --output=knn-mimic-1-gower-tsicu.out # Std output and error log
+
+pwd; hostname; date
+
+module load R
+
+echo "Running `kmg_tsicu` on one core..."
+
+Rscript ~/lastfirst/scripts/knn-mimic-1-gower-tsicu.r
+
+date
